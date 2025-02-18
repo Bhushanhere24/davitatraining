d293b6b6ecdc28cd8e44e8cb9ca0388ff62afc72
Bhushanhere24 <55726598+Bhushanhere24@users.noreply.github.com>
2025-02-18 15:44:01 +0000

    Update CitiIdValidation.java

--- CitiIdValidation.java
+++ CitiIdValidation.java
@@ -5,8 +5,8 @@ public class CitiIdValidation {
 //    Generate method to validate Citi ID should be starting with CT + 8 digits
     public static boolean isValidCitiId(String citiId) {
         if (citiId == null || citiId.length() != 10) {
+            Syste.out.printl("Id not Valid");
             return false;
-            //return false
         }
         return citiId.startsWith("CT") && citiId.substring(2).matches("\\d{8}");
     }
